34e60346728b4a28d438fb717ba5e371cc99cd4a
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2025-01-17 13:29:22 +0300

    upadted the user detailled routes with param

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // config: {
-  //   api: {
-  //     bodyParser: false,
-  //   },
-  // },
+  config: {
+    api: {
+      bodyParser: false,
+    },
+  },
   images: {
     remotePatterns: [
       {
